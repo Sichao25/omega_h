18d3215ef97db17cd0e2f96aa2207f2a73dd2079
Cameron Smith <smithc11@rpi.edu>
2024-08-29 14:23:07 -0400

    fix failing meshsim2osh test
    Comparing meshes between builds was resulting in failed diffs. Looking at the 'old' gold mesh that was stored in the repo and a 'new' mesh, the global orderings (generated by osh_reorder) were different. It seems more likely that changing the simmetrix library version caused the differences.

--- src/meshsim2osh.cpp
+++ src/meshsim2osh.cpp
@@ -3,8 +3,20 @@
 #include "Omega_h_cmdline.hpp"
 #include "Omega_h_file.hpp"
 #include "Omega_h_mesh.hpp"
-
 #include "Omega_h_build.hpp"
+#include "Omega_h_for.hpp"
+
+void localToGlobal(Omega_h::Mesh* mesh, int entDim) {
+  OMEGA_H_CHECK(entDim >= 0 && entDim <= mesh->dim());
+  std::string kernelName = "local_to_global_" + entDim;
+  Omega_h::Write<Omega_h::GO> global(mesh->nents(entDim));
+  Omega_h::parallel_for(kernelName.c_str(), mesh->nents(entDim),
+    OMEGA_H_LAMBDA(int i) { global[i] = static_cast<Omega_h::GO>(i); }
+  );
+  mesh->add_tag<Omega_h::GO>(entDim, "global", 1, Omega_h::read(global));
+}
+
+
 int main(int argc, char** argv) {
   auto lib = Omega_h::Library(&argc, &argv);
   auto comm = lib.world();
@@ -27,13 +39,16 @@ int main(int argc, char** argv) {
   }
   auto isMixed = Omega_h::meshsim::isMixed(mesh_in, model_in);
   std::cerr << "isMixed " << isMixed << "\n";
-  //TODO - call the correct reader (mixed vs mono)
   if( !isMixed ) {
     auto mesh = Omega_h::meshsim::read(mesh_in, model_in, numbering_in, comm);
+    //convert the local numbering to global
+    //needed for oshdiff, see github issue #40
+    for(int entDim=0; entDim<=mesh.dim(); entDim++) {
+      localToGlobal(&mesh,entDim);
+    }
     Omega_h::binary::write(mesh_out, &mesh);
   } else {
     auto mesh = Omega_h::meshsim::readMixed(mesh_in, model_in, comm);
   }
-
   return 0;
 }
