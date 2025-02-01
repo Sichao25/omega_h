8135e32d19a8767064fcb0cac7be1cbd6c071252
Angelyr <scardking@gmail.com>
2025-02-01 13:27:21 -0800

    fixed perlmutter test

--- .github/workflows/perlmutter/install.sh
+++ .github/workflows/perlmutter/install.sh
@@ -4,51 +4,37 @@ branch=$1
 
 cd $SCRATCH/globus-compute/omega_h-test
 
-export root=$PWD
-module load PrgEnv-gnu
-module load cudatoolkit
-module load cmake
-
-export kk=$root/build-kokkos/install   # This is where kokkos will be (or is) installed
-export oh=$root/build-omega_h/install  # This is where omega_h will be (or is) installed
-export CMAKE_PREFIX_PATH=$kk:$kk/lib64/cmake:$oh:$CMAKE_PREFIX_PATH
-export MPICH_CXX=$root/kokkos/bin/nvcc_wrapper
-
-export SLURM_CPU_BIND="cores"
-
-
-# #kokkos
-# rm ${kk%%install} -rf
+# # kokkos
 # rm kokkos -rf
-# git clone -b 4.2.00 https://github.com/kokkos/kokkos.git
-# cmake -S kokkos -B ${kk%%install} \
-#   -DCMAKE_INSTALL_PREFIX=$kk \
+# rm build-kokkos -rf
+# git clone -b 4.5.00 https://github.com/kokkos/kokkos.git
+# cmake -S kokkos -B build-kokkos \
+#   -DCMAKE_INSTALL_PREFIX=build-kokkos/install \
 #   -DCMAKE_BUILD_TYPE="Release" \
-#   -DCMAKE_CXX_COMPILER=$root/kokkos/bin/nvcc_wrapper \
+#   -DCMAKE_CXX_COMPILER=$PWD/kokkos/bin/nvcc_wrapper \
 #   -DKokkos_ARCH_AMPERE80=ON \
 #   -DKokkos_ENABLE_SERIAL=ON \
 #   -DKokkos_ENABLE_OPENMP=off \
 #   -DKokkos_ENABLE_CUDA=on \
 #   -DKokkos_ENABLE_CUDA_LAMBDA=on \
 #   -DKokkos_ENABLE_DEBUG=off
-# cmake --build ${kk%%install} -j 24 --target install
+# cmake --build build-kokkos -j 24 --target install
 
-#omegah
-rm ${oh%%install} -rf
+# omegah
 rm omega_h -rf
+rm build-omega_h -rf
 git clone https://github.com/SCOREC/omega_h.git
 cd omega_h && git checkout $branch && cd -
-cmake -S omega_h -B ${oh%%install} \
-  -DCMAKE_INSTALL_PREFIX=$oh \
+cmake -S omega_h -B build-omega_h \
+  -DCMAKE_INSTALL_PREFIX=build-omega_h/install \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_SHARED_LIBS=off \
   -DOmega_h_USE_Kokkos=ON \
   -DOmega_h_USE_CUDA=on \
   -DOmega_h_CUDA_ARCH=80 \
   -DOmega_h_USE_MPI=on  \
-  -DMPIEXEC_EXECUTABLE=srun \
   -DBUILD_TESTING=on  \
   -DCMAKE_C_COMPILER=cc \
   -DCMAKE_CXX_COMPILER=CC \
-  -DKokkos_PREFIX=$kk/lib64/cmake
-cmake --build ${oh%%install} -j 24 --target install
+  -DKokkos_PREFIX=$PWD/build-kokkos/install/lib64/cmake
+cmake --build build-omega_h -j 24 --target install
--- .github/workflows/perlmutter/run.sh
+++ .github/workflows/perlmutter/run.sh
@@ -2,15 +2,7 @@
 
 name=omega_h
 
-cd $SCRATCH/globus-compute/$name-test
+cd $SCRATCH/globus-compute/$name-test/build-$name
 
-export root=$PWD
-module load PrgEnv-gnu
-module load cudatoolkit
-module load cmake
-export MPICH_CXX=$root/kokkos/bin/nvcc_wrapper
-export SLURM_CPU_BIND="cores"
-
-cd build-$name
 salloc --time 00:20:00 --constrain=gpu --qos=interactive --nodes=1 --ntasks-per-node=40 --cpus-per-task=1 --gpus=1 --account=m4564 ctest
 cat $PWD/Testing/Temporary/LastTest.log
